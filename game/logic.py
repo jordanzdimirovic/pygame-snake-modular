e376e57d540acd9c5944dfef12f86bb46427369b
Jordan Zdimirovic <jordan.zdimirovic@gmail.com>
2022-06-29 20:40:58 +1000

    ✓ Added templates (with documentation) for render and logic functions.

--- game/logic.py
+++ game/logic.py
@@ -5,6 +5,67 @@
         - Must maintain their names
     
     This file's purpose is to be modified by a given student,
-    such that they can implement the logic, whilst maintaining
+    such that they can implement the logic, whilst abstracting away
     the rest of the implementation (i.e. rendering, PyGame things, etc.)
 """
+
+#region LOGIC VARIABLES
+
+"""
+A list of lists that represents
+each part of the snake.
+The left-most sub-list is the head,
+whilst the right-most sub-list is the tail.
+[[head_x, head_y], ..., [tail_x, tail_y]]
+Example: 
+"""
+snake = []
+
+"""
+Is the snake alive or dead?
+"""
+is_alive = True
+
+"""
+What direction is the snake
+currently moving?
+
+Possible values:
+    - "UP"
+    - "DOWN"
+    - "LEFT"
+    - "RIGHT"
+"""
+move_direction = "UP"
+
+"""
+Represents the score that
+the player has currently acquired.
+"""
+player_score = 0
+
+#endregion
+
+#region LOGIC FUNCTIONS
+
+#region KEYBOARD ARROW BUTTON LOGIC
+
+# Runs when the left arrow is pressed
+def on_left_arrow():
+    pass
+
+# Runs when the right arrow is pressed
+def on_right_arrow():
+    pass
+
+# Runs when the up arrow is pressed
+def on_up_arrow():
+    pass
+
+# Runs when the down arrow is pressed
+def on_down_arrow():
+    pass
+
+#endregion 
+
+#endregion
--- game/render.py
+++ game/render.py
@@ -1,3 +1,48 @@
 """
     Everything related to rendering will be within this script.
 """
+
+# LIBRARY IMPORTS
+# ---------------
+
+import pygame
+
+# ---------------
+#  END LIBRARIES
+
+def setup_screen(screen_size, window_name = "Snake Game"):
+    """
+        Sets up everything PyGame related.
+    """
+    pass
+
+
+def draw_overlay(score):
+    """
+        Draws the overlay, including any text
+        (such as score, etc).
+    """
+    pass
+
+
+def draw_grid():
+    """
+        Draws the game grid.
+    """
+    pass
+
+
+def draw_snake(snake, colour):
+    """
+        Draws the snake on the
+        grid, given the list that 
+        represents its parts.
+    """
+    pass
+
+
+def draw_apple():
+    """
+        Draws the apple on the grid.
+    """
+    pass
